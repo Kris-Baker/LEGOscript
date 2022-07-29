08f375012329f57ec3ffefa203929384fa12515c
Kris Baker <89749026+Kris-Baker@users.noreply.github.com>
2022-07-29 18:06:22 +0100

    Parser now reads from console, rather than hardcoded strings.
    TODO: allow the user to specify a flag -f to read from a plaintext file instead of from the console. 
    TODO: fix part of code that is treating EOF as an error.
    TODO: add validation for the disallowment of uppercase characters
    TODO: only allow s and c primatives to be treat as valid (more will be added later however)

--- parser.c
+++ parser.c
@@ -1,56 +1,35 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
-void parse_line(const char *line, const char *end){
-    // While we have not reached the end of the line
-    while (line < end){
-        const char *start = line;
-
-        // Find the end of the current token, which ends at ',' or ')'
-        line = strpbrk(line, ",)");
-
-        // If there is no end, we are done
-        if (!line) break;
-
-        // This is the length of the token
-        size_t length = line - start;
-
-        // Print the token and a comma
-        printf("%.*s,", (int)length, start);
-
-        // Done with token, try to find the next one
-        line++;
-    }
-    printf("\n");
-}
-
-void parse_text(const char *text){
-    // While there is text to parse
-    while (*text){
-        // Find end of line
-        const char *end_of_line = strchr(text, '\n');
-
-        // If there is no end of line, we are done
-        if (!end_of_line) break;
-
-        // Otherwise, parse the line
-        parse_line(text + 1, end_of_line - 1);
-
-        // And go to next line
-        text = end_of_line + 1;
+int ReadAndValidateLine( char* pzBuffer , int iLimit ) {
+    int iChar, iLen=0, iParenthesisCount=0;
+    while (iChar=getchar()) {
+        if (iLen>=iLimit) { iChar = '\n'; }
+        switch (iChar) {
+        case '(':
+          iParenthesisCount++;
+          break;
+        case ')':
+          iParenthesisCount--;
+          break;
+        case EOF:
+        case '\n':
+          pzBuffer[iLen]=0;
+          if (iParenthesisCount) {
+              puts("Mistmatch parenthesis");
+              return 0;
+          }
+          return iLen;
+        }
+        pzBuffer[iLen++] = iChar;
     }
 }
 
-int main(){
-    parse_text("(1,0,1)\n(3,3)\n");
-  
-  // 0 = pinhole
-  // 1 = axle hole
-  // 2 = pin
-  // 3 = axle
-  // TODO convert 0,1,2,3 into PIH,AXH,PI,AX
-  // TODO get input from user, not as a hardcoded string literal
-  
-    return 0;
+int main() {
+    char zLine[4096];
+    if (!ReadAndValidateLine( zLine , 4095 )) {
+        puts("error");
+    } else {
+        puts("ok");
+        printf("'%s'\n",zLine);
+    }
 }
+
